3e3782928ce5fdb362062be34b4c6c65bb4163f8
psychobolt <psychobolt@gmail.com>
2017-05-19 16:17:07 -0700

    Make gauge work with data set

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# FakeNewsExtension
+# Fake News Chrome Extension
+
+[![logo](https://cloud.githubusercontent.com/assets/560721/26270165/1c1753f0-3cae-11e7-8cb3-8cc4e95f1484.png)]
+
+This extension displays a credibility percentage of a new source currently opened in the Chrome tab.
+
+Our extension utilizes [OpenSources](https://opensources.co) data set for detecting Fake News [domains](http://github.com/BigMcLargeHuge/opensources/blob/master/sources/sources.csv). 
 
 ## Testing
 
 1. Download or clone the repository (project).
 2. Launch Chrome
 3. Under Settings > Extensions, check Developer Mode. Some buttons should appear.
-4. Click "Load unpacked extension"
+4. Click "Load unpacked extension". The extension should appear as an Chrome action button.
 5. Select the projet folder. The extension should be loaded.
+6. Visting a website e.g. wikileaks.org
+7. Click on the action button.
 
--- popup.js
+++ popup.js
@@ -1,114 +1,91 @@
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 (function ($) {
 
-var gauge_value = 0
+  var gauge_value = 0,
+      gauge_color;
 
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+  /**
+   * Get the current URL.
+   *
+   * @param {function(string)} callback - called when the URL of the current tab
+   *   is found.
+   */
+  function getCurrentTabUrl(callback) {
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+    chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0];
+      var url = tab.url;
+      console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  $.getJSON('sources.json', function (sources) {
-    console.log(sources);
-    // do something with the data
-  });
-  getCurrentTabUrl(function(url) {
-    renderStatus('Performing News Analysis for:\n' + url);
-  });
-  
-  if (gauge_value <= 20) {
-    gauge_color =  '#0000FF'
-  } else if (gauge_value <= 40) {
-    gauge_color =  '#FF0099'
-  } else if (gauge_value <= 60) {
-    gauge_color = '#FF00FF' 
-  } else if (gauge_value <= 80) {
-    gauge_color =  '#9900FF'
-  } else {
-    gauge_color = '#FF0000'
+      callback(url);
+    });
   }
 
-  var opts = {
-    angle: 0, // The span of the gauge arc
-    lineWidth: 0.39, // The line thickness
-    radiusScale: 0.78, // Relative radius
-    pointer: {
-      length: 0.66, // // Relative to gauge radius
-      strokeWidth: 0.062, // The thickness
-      color: '#919191' // Fill color
-    },
-    limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
-    limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
-    colorStart: '#CF0202',   // Colors
-    colorStop: gauge_color,    // just experiment with them
-    strokeColor: '#CFCFCF',  // to see which ones work best for you
-    generateGradient: true,
-    highDpiSupport: true     // High resolution support
-  };
-  var target = document.getElementById('foo'); // your canvas element
-  var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-  gauge.maxValue = 100; // set max gauge value
-  gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-  gauge.animationSpeed = 32; // set animation speed (32 is default value)
-  gauge.set(gauge_value); // set actual value
-});
+  function ChangePrediction(callback) {
+      gauge_value = gauge_value + 20
+      if (gauge_value > 100){
+        gauge_value = 0
+      }
+      console.assert(typeof gauge_value == "number", 'tab.gauge_value should be a int');
 
-function ChangePrediction(callback) {
-    gauge_value = gauge_value + 20
-    if (gauge_value > 100){
-      gauge_value = 0
-    }
-    console.assert(typeof gauge_value == "number", 'tab.gauge_value should be a int');
+      callback(gauge_value);
+  }
 
-    callback(gauge_value);
-}
+  function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
+  }
 
-document.addEventListener('click', function() {
-  ChangePrediction(function(gauge_value) {
-    gauge_value = gauge_value + 20
-    if (gauge_value > 100) {
-      gauge_value = 0
+  function render (url, sources) {
+    renderStatus('Analysis for:\n' + url);
+    
+    console.log(sources);
+
+    match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im);
+    source = sources[match[1]];
+
+    var target = document.getElementById('foo'); // your canvas element
+
+    if (match[0] === 'chrome') {
+      var context = target.getContext('2d');
+      context.fillStyle = 'white';
+      context.font = '15px serif';
+      context.fillText('Visit a news source.', 90, 90);
+      return;
     }
-    if (gauge_value <= 20) {
-      gauge_color =  '#0000FF'
-      alert('This Window would show the results of the machine learning prediction, as well as why we may have found an article to be fake.')
-    } else if (gauge_value <= 40) {
-      gauge_color =  '#FF0099'
-    } else if (gauge_value <= 60) {
-      gauge_color = '#FF00FF' 
-    } else if (gauge_value <= 80) {
-      gauge_color =  '#9900FF'
-    } else {
-      gauge_color = '#FF0000'
+
+    if (source) {
+      switch (source.type) {
+        case 'unreliable':
+          gauge_value = 20;
+          gauge_color = '#0000FF';
+          break;
+        case 'rumor':
+          gauge_value = 40;
+          gauge_color = '#FF0099';
+          break;
+        case 'clickbait':
+          gauge_value = 60;
+          gauge_color = '#FF00FF';
+          break;
+        case 'bias':
+        case 'junksci':
+          gauge_value = 80;
+          gauge_color = '#9900FF';
+          break;
+        case 'fake':
+        case 'hate':
+        case 'fake news':
+        case 'conspiracy':
+           gauge_value = 100;
+           gauge_color = '#FF0000';
+           break;
+        
+      }
     }
+
     var opts = {
       angle: 0, // The span of the gauge arc
       lineWidth: 0.39, // The line thickness
@@ -126,13 +103,23 @@ document.addEventListener('click', function() {
       generateGradient: true,
       highDpiSupport: true     // High resolution support
     };
-    var target = document.getElementById('foo'); // your canvas element
     var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
     gauge.maxValue = 100; // set max gauge value
     gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
     gauge.animationSpeed = 32; // set animation speed (32 is default value)
     gauge.set(gauge_value); // set actual value
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {  
+    $.getJSON('sources.json', function (sources) {
+      getCurrentTabUrl(function (url) {
+        render(url, sources);
+      });
+    });
+
+    $('#more-info').on('click', function () {
+      window.open('https://github.com/MachineLearningFakeNews/ChromeExtension')
+    });
   });
-});
 
 }(jQuery));
